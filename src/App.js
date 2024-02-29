8c9668e080e9143b0014b34ff03f146b429e7a34
harsh-purecode <159994935+harsh-purecode@users.noreply.github.com>
2024-02-29 20:12:15 +0530

    my-profile

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 // import Routers from "./Routers";
 import Default from "./components/common/Default";
 import Layout from "./components/common/layout";
+import UserProfile from "./pages/UserProfile";
 
 
 function App() {
   return (
       <Default>
           <>
-          <h1>hasd</h1>
-              <Layout />
-
+            <Layout>
+              <UserProfile />
+            </Layout>
           </>
       </Default>
   );
