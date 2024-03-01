64522f32302f69c7ab22e84521cb2e61fba98b70
harsh-purecode <159994935+harsh-purecode@users.noreply.github.com>
2024-03-01 17:23:12 +0530

    update-pass

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-// import Toaster from "./components/Helpers/Toaster";
-// import Routers from "./Routers";
-import Default from "./components/common/Default";
-import Layout from "./components/common/layout";
 
+import UpdatePassword from "./pages/UpdatePassword/UpdatePassword";
 
 function App() {
   return (
-      <Default>
-          <>
-          <h1>hasd</h1>
-              <Layout />
-
-          </>
-      </Default>
+            <UpdatePassword />
   );
 }
 
