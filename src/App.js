d767b176866c6886f32420e4291f1ddc4be610ec
harsh-purecode <159994935+harsh-purecode@users.noreply.github.com>
2024-03-01 14:59:07 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-// import Toaster from "./components/Helpers/Toaster";
-// import Routers from "./Routers";
-import Default from "./components/common/Default";
-import Layout from "./components/common/layout";
 
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-      <Default>
-          <>
-          <h1>hasd</h1>
-              <Layout />
-
-          </>
-      </Default>
+            <SignUp />
   );
 }
 
