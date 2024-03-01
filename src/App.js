b7539bf83e59cd7693fd4a8d67225a54673c2916
harsh-purecode <159994935+harsh-purecode@users.noreply.github.com>
2024-03-01 15:22:06 +0530

    forget-pass

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-// import Toaster from "./components/Helpers/Toaster";
-// import Routers from "./Routers";
-import Default from "./components/common/Default";
-import Layout from "./components/common/layout";
 
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-      <Default>
-          <>
-          <h1>hasd</h1>
-              <Layout />
-
-          </>
-      </Default>
+            <ForgotPassword />
   );
 }
 
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import drawerReducer from "./drawer";
-
-export default configureStore({
-  reducer: {
-    drawer: drawerReducer,
-  },
-});
