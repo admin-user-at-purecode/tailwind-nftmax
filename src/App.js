7953f7fcd3c65e56c235c1a577a9359fdb5ecd7a
harsh-purecode <159994935+harsh-purecode@users.noreply.github.com>
2024-03-01 11:05:44 +0530

    settingsdone

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 // import Routers from "./Routers";
 import Default from "./components/common/Default";
 import Layout from "./components/common/layout";
+import Settings from "./pages/Settings";
 
 
 function App() {
   return (
       <Default>
           <>
-          <h1>hasd</h1>
-              <Layout />
-
+            <Layout>
+              <Settings />
+            </Layout>
           </>
       </Default>
   );
