7f490ec5b14f1df8c5445475d4dbe1eed892259b
harsh-purecode <159994935+harsh-purecode@users.noreply.github.com>
2024-03-01 18:10:29 +0530

    verify-you

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-// import Toaster from "./components/Helpers/Toaster";
-// import Routers from "./Routers";
-import Default from "./components/common/Default";
-import Layout from "./components/common/layout";
 
+import VerifyYou from "./pages/VerifyYou/VerifyYou";
 
 function App() {
   return (
-      <Default>
-          <>
-          <h1>hasd</h1>
-              <Layout />
-
-          </>
-      </Default>
+            <VerifyYou />
   );
 }
 
